d86bbfbdd669d17c899918995bcec671566c167c
Mujahid Hussain <hmujahid08@gmail.com>
2024-10-29 23:00:42 +0500

    Fix typo in console log message: "DATABSE" -> "DATABASE"

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("DATABSE CONNECTED✔✔✔");
+    console.log("DATABASE CONNECTED✔✔✔");
   } catch (error) {
     console.log(error);
   }
