20112e4fff3e769176d9f3d1ee3f4437ceea5dcf
Mujahid Hussain <hmujahid08@gmail.com>
2024-10-29 22:38:11 +0500

    "Added import and call to connectDB function in app.js"

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import Fastify from "fastify";
 import "dotenv/config";
+import connectDB from "./src/config/db.js";
 
 const start = async () => {
   const app = Fastify();
+  connectDB();
   const PORT = process.env.PORT || 8080;
   app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
     if (err) {
