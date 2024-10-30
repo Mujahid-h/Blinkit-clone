3ddbaaa50398e21fe632006d28a43669230da8ee
Mujahid Hussain <hmujahid08@gmail.com>
2024-10-30 11:57:47 +0500

    Setup the admin

---
+++ server/src/config/admin.js
@@ -0,0 +1,39 @@
+import AdminJS from "adminjs";
+import * as AdminJSMongoose from "@adminjs/mongoose";
+import * as Models from "../models/index.js";
+
+AdminJS.registerAdapter(AdminJSMongoose);
+
+export const admin = new AdminJS({
+  resources: [
+    {
+      resource: Models.Customer,
+      options: {
+        listProperties: ["phone", "role", "isActivated"],
+        filterProperties: ["phone", "role"],
+      },
+    },
+    {
+      resource: Models.DeliveryPartner,
+      options: {
+        listProperties: ["email", "role", "isActivated"],
+        filterProperties: ["email", "role"],
+      },
+    },
+    {
+      resource: Models.Admin,
+      options: {
+        listProperties: ["email", "role", "isActivated"],
+        filterProperties: ["email", "role"],
+      },
+    },
+    {
+      resource: Models.Branch,
+    },
+  ],
+  branding: {
+    companyName: "Mujahid.Ltd",
+    withMadeWithLove: false,
+  },
+  rootPath: "/admin",
+});
