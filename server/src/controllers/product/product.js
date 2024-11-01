b8b33d4fd9e63a44d95e49ea9e15dd3d8ca200da
Mujahid Hussain <hmujahid08@gmail.com>
2024-11-01 16:11:05 +0500

    Added category and product controllers

---
+++ server/src/controllers/product/product.js
@@ -0,0 +1,18 @@
+import { Product } from "../../models/Product.js";
+
+export const getProductsByCategoriesId = async (req, reply) => {
+  try {
+    const { categoryId } = req.params;
+
+    const products = await Product.find({ category: categoryId })
+      .select("-category")
+      .exec();
+
+    return reply.send({
+      message: "Products fetched successfully!",
+      products,
+    });
+  } catch (error) {
+    return reply.status(500).send({ message: "An error occured" });
+  }
+};
