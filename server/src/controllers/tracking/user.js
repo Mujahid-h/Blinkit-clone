3c52006b64333c30f99d24377b83474d45717889
Mujahid Hussain <hmujahid08@gmail.com>
2024-11-01 15:38:52 +0500

    Added update user controller in auth

---
+++ server/src/controllers/tracking/user.js
@@ -0,0 +1,35 @@
+import { Customer, DeliveryPartner } from "../../models/User.js";
+
+export const updateUser = async (req, reply) => {
+  try {
+    const { userId } = req.userId;
+    const updatedData = req.body;
+
+    let user =
+      (await Customer.findById(userId)) ||
+      (await DeliveryPartner.findById(userId));
+
+    if (!user) {
+      return reply.status(404).send({ message: "User not found" });
+    }
+
+    const userModel = user.role === "Customer" ? Customer : DeliveryPartner;
+
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userId,
+      { $set: updatedData },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return reply.status(404).send({ message: " Updated user not found" });
+    }
+
+    return reply.send({
+      message: "User updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    return reply.status(500).send({ messgae: "An error occured" });
+  }
+};
