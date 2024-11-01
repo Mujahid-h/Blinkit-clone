8b38557b9325e4827a8da107ed9b4823aaf9099a
Mujahid Hussain <hmujahid08@gmail.com>
2024-11-01 17:37:28 +0500

    "Removed curly braces from import statements in category and product controllers; updated route registration in index.js"

--- server/src/controllers/product/product.js
+++ server/src/controllers/product/product.js
@@ -1,4 +1,4 @@
-import { Product } from "../../models/Product.js";
+import Product from "../../models/Product.js";
 
 export const getProductsByCategoriesId = async (req, reply) => {
   try {
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,10 @@
 import { authRoutes } from "./auth.js";
+import { categoriesRoutes, productRoutes } from "./product.js";
 
 const prefix = "/api";
 
 export const registerRoutes = async (fastify) => {
   fastify.register(authRoutes, { prefix: prefix });
+  fastify.register(productRoutes, { prefix: prefix });
+  fastify.register(categoriesRoutes, { prefix: prefix });
 };
