50151909143e351297289f10343156290fb1977e
Mujahid Hussain <hmujahid08@gmail.com>
2024-11-02 11:15:30 +0500

    "Updated order controller and added new routes for orders"

--- server/src/controllers/order/order.js
+++ server/src/controllers/order/order.js
@@ -7,17 +7,17 @@ import {
 
 export const createOrder = async (req, reply) => {
   try {
-    const { userId } = req.userId;
+    const { userId } = req.user;
     const { items, branch, totalPrice } = req.body;
 
     const customerData = await Customer.findById(userId);
-    const branchData = await Branch.find(branch);
+    const branchData = await Branch.findById(branch);
 
     if (!customerData) {
       return reply.code(404).send({ message: "Customer not found" });
     }
 
-    const newOrder = new Order.create({
+    const newOrder = new Order({
       customer: userId,
       items: items.map((item) => ({
         id: item.id,
@@ -40,11 +40,12 @@ export const createOrder = async (req, reply) => {
 
     const savedOrder = await newOrder.save();
 
-    return replystatus(201).send({
+    return reply.status(201).send({
       message: "Order created successfully!",
       savedOrder,
     });
   } catch (error) {
+    console.log(error);
     return reply.status(500).send({ message: "Failed to create order" });
   }
 };
@@ -109,6 +110,44 @@ export const updateOrderStatus = async (req, reply) => {
 
     return reply.send(order);
   } catch (error) {
-    return reply.status(500).send({ message: "Failed to create order" });
+    return reply.status(500).send({ message: "Failed to update order status" });
+  }
+};
+
+export const getOrders = async (req, reply) => {
+  try {
+    const { customerId, deliveryPartnerId, status, branchId } = req.query;
+
+    let query = {};
+
+    if (status) query.status = status;
+    if (customerId) query.customer = customerId;
+    if (deliveryPartnerId) {
+      (query.deliveryPartner = deliveryPartnerId), (query.branch = branchId);
+    }
+
+    const orders = await Order.find(query).populate(
+      "customer branch items.item deliveryPartner"
+    );
+
+    return reply.send(orders);
+  } catch (error) {
+    return reply.status(500).send({ message: "Failed to retrieve orders" });
+  }
+};
+
+export const getOrderById = async (req, reply) => {
+  try {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId).populate(
+      "customer branch items.item deliveryPartner"
+    );
+
+    if (!order) return reply.status(404).send({ message: "Order not found" });
+
+    return reply.send(order);
+  } catch (error) {
+    return reply.status(500).send({ message: "Failed to retrieve orders" });
   }
 };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,4 +1,5 @@
 import { authRoutes } from "./auth.js";
+import { orderRoutes } from "./order.js";
 import { categoriesRoutes, productRoutes } from "./product.js";
 
 const prefix = "/api";
@@ -7,4 +8,5 @@ export const registerRoutes = async (fastify) => {
   fastify.register(authRoutes, { prefix: prefix });
   fastify.register(productRoutes, { prefix: prefix });
   fastify.register(categoriesRoutes, { prefix: prefix });
+  fastify.register(orderRoutes, { prefix: prefix });
 };
