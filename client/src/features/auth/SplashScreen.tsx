8412988ce85c61d6281b1bd779a3eb0094a8b38a
Mujahid Hussain <hmujahid08@gmail.com>
2024-11-06 22:40:48 +0500

    "Updated app.json, package-lock.json, and package.json files; added geolocation and axios dependencies; modified SplashScreen.tsx to request location permission and fetch user location"

--- client/src/features/auth/SplashScreen.tsx
+++ client/src/features/auth/SplashScreen.tsx
@@ -1,11 +1,42 @@
-import { StyleSheet, Text, View, Pressable, Image } from "react-native";
-import React from "react";
-import { router } from "expo-router";
+import { StyleSheet, Text, View, Image, Alert } from "react-native";
+import React, { useEffect } from "react";
 import { Colors } from "@utils/Constants";
 import Logo from "@assets/images/splash_logo.jpeg";
 import { screenHeight, screenWidth } from "@utils/Scaling";
+import * as Location from "expo-location";
+import { useAuthStore } from "@state/authStore";
 
 const SplashScreen = () => {
+  const { user, setUser } = useAuthStore();
+
+  useEffect(() => {
+    const fetchUserLocation = async () => {
+      try {
+        // Request permission for foreground location access
+        const { status } = await Location.requestForegroundPermissionsAsync();
+
+        if (status !== "granted") {
+          Alert.alert(
+            "Location Permission Denied",
+            "We need location permission to improve your experience."
+          );
+          return;
+        }
+
+        // Optional: Request background location permission if needed
+        await Location.requestBackgroundPermissionsAsync();
+
+        // Fetch the user's current location
+        const location = await Location.getCurrentPositionAsync({});
+        console.log("User Location:", location);
+      } catch (error) {
+        Alert.alert("Error", "Could not fetch location. Please try again.");
+      }
+    };
+
+    fetchUserLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={Logo} style={styles.logo} />
@@ -22,7 +53,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primary,
   },
-
   logo: {
     width: screenWidth * 0.7,
     height: screenHeight * 0.7,
