8560a927fbd5d71131a946b104d8a446cf2ca95e
Mujahid Hussain <hmujahid08@gmail.com>
2024-11-11 15:31:59 +0500

    Loaded the fonts

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,22 @@
 import "react-native-gesture-handler";
 import Navigation from "@navigation/Navigation";
+import * as Font from "expo-font";
+import { useState, useEffect } from "react";
 
 export default function Index() {
-  return <Navigation />;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    Font.loadAsync({
+      "Okra-Regular": require("@assets/fonts/Okra-Regular.ttf"),
+      "Okra-Medium": require("@assets/fonts/Okra-Medium.ttf"),
+      "Okra-MediumLight": require("@assets/fonts/Okra-MediumLight.ttf"),
+      "Okra-Bold": require("@assets/fonts/Okra-Bold.ttf"),
+      "Okra-ExtraBold": require("@assets/fonts/Okra-ExtraBold.ttf"),
+    }).then(() => setFontsLoaded(true));
+  }, []);
+
+  if (fontsLoaded) {
+    return <Navigation />;
+  }
 }
--- frontend/src/features/auth/CustomerLogin.tsx
+++ frontend/src/features/auth/CustomerLogin.tsx
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
   logo: {
     width: 50,
     height: 50,
-    borderRadius: 25,
+    borderRadius: 20,
     marginVertical: 10,
   },
 });
