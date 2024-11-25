9247a3a1fa5573b100d15cb42c028c67978aea65
Mujahid Hussain <hmujahid08@gmail.com>
2024-11-25 15:53:42 +0500

    "Added react-native-reanimated-carousel dependency and implemented carousel component in AdCarousal.tsx"

--- frontend/src/features/dashboard/AdCarousal.tsx
+++ frontend/src/features/dashboard/AdCarousal.tsx
@@ -1,10 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React, { FC } from "react";
+import { useSharedValue } from "react-native-reanimated";
+import { screenWidth } from "@utils/Scaling";
+import Carousel from "react-native-reanimated-carousel";
 
 const AdCarousal: FC<{ adData: any }> = ({ adData }) => {
+  const progressValue = useSharedValue(0);
+  const baseOptions = {
+    vertical: false,
+    width: screenWidth,
+    height: screenWidth * 0.5,
+  };
   return (
     <View style={{ left: -10, marginVertical: 20 }}>
-      <Text>AdCarousal</Text>
+      <Carousel
+        {...baseOptions}
+        loop
+        pagingEnabled
+        snapEnabled
+        autoPlay
+        autoPlayInterval={3000}
+        data={adData}
+        mode="parallax"
+        modeConfig={{
+          parallaxScrollingOffset: 0.94,
+          parallaxScrollingScale: 0,
+        }}
+        renderItem={({ item }: any) => {
+          return <Image source={item} style={styles.img} />;
+        }}
+      />
     </View>
   );
 };
