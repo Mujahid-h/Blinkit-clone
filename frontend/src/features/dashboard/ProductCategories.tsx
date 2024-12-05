dd09dd8359137473335c57140d985371da82a839
Mujahid Hussain <hmujahid08@gmail.com>
2024-12-05 22:47:43 +0500

    "Added import of StyleSheet and created empty styles object in ProductCategories component"

--- frontend/src/features/dashboard/ProductCategories.tsx
+++ frontend/src/features/dashboard/ProductCategories.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const ProductCategories = () => {
@@ -10,3 +10,5 @@ const ProductCategories = () => {
 };
 
 export default ProductCategories;
+
+const styles = StyleSheet.create({});
