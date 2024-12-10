712642900944d9afb4a385cc05fbe86bba9a45c6
Mujahid Hussain <hmujahid08@gmail.com>
2024-12-10 19:45:15 +0500

    `Update import statement in ProductCategories.tsx to include type annotation`

--- frontend/src/features/category/ProductCategories.tsx
+++ frontend/src/features/category/ProductCategories.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { FC } from "react";
 
-const ProductCategories = () => {
+const ProductCategories: FC = () => {
   return (
     <View style={styles.container}>
       <Text>ProductCategories</Text>
