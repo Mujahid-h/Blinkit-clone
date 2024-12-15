f11705ffb4031bdae524df9e7396c301cfa2dd91
Mujahid Hussain <hmujahid08@gmail.com>
2024-12-15 20:07:52 +0500

    "Removed unused import from Sidebar.tsx"

--- frontend/src/features/category/Sidebar.tsx
+++ frontend/src/features/category/Sidebar.tsx
@@ -1,10 +1,4 @@
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
 import React, { FC, useEffect, useRef } from "react";
 import Animated, {
   runOnJS,
